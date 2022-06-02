ba65d5422e42855c28681d0a5ebd670a87059eed
Kathleen Wims <bewimsical@gmail.com>
2022-06-02 00:05:21 -0400

    Updated server

--- server.js
+++ server.js
@@ -22,159 +22,196 @@ const server = http.createServer((req, res) => {
     case "/":
       createfile("index.html", "text/html");
       break;
-    case "/otherpage.html":
-      createfile("otherpage.html", "text/html");
-      break;
-    case "/otherotherpage":
-      createfile("otherotherpage.html", "text/html");
-      break;
-    case "/tic-tac-toe/index.html":
-      createfile("tic-tac-toe/index.html", "text/html");
-      break;
-    case "/rock-paper-scissors/index.html":
-      createfile("rock-paper-scissors/index.html", "text/html");
-      app.use(express.static("images"));
-      app.use("./images", express.static("images"));
-      break;
-    case "/rock-paper-scissors/images/rock.png":
-      createfile("rock-paper-scissors/images/rock.png", "image/gif");
-      break;
-    case "/rock-paper-scissors/images/paper.png":
-      createfile("rock-paper-scissors/images/paper.png", "image/gif");
-      break;
-    case "/rock-paper-scissors/images/scissors.png":
-      createfile("rock-paper-scissors/images/scissors.png", "image/gif");
-      break;
-    case "/images/logo.png":
-      createfile("images/logo.png", "image/gif");
-      break;
-    case "/assets/css/images/header.svg":
-      createfile("assets/css/images/header.svg", "image/svg+xml");
-      break;
-    case "/images/bee.png":
-      createfile("images/bee.png", "image/gif");
-      break;
-    case "/images/rock.png":
-      createfile("images/rock.png", "image/gif");
-      break;
-    case "/images/snake.png":
-      createfile("images/snake.png", "image/gif");
-      break;
-    case "/images/tic.png":
-      createfile("images/tic.png", "image/gif");
-      break;
-    case "/api":
-      res.writeHead(200, { "Content-Type": "application/json" });
+    // case "/otherpage.html":
+    //   createfile("otherpage.html", "text/html");
+    //   break;
+    // case "/otherotherpage":
+    //   createfile("otherotherpage.html", "text/html");
+    //   break;
+    // case "/tic-tac-toe/index.html":
+    //   createfile("tic-tac-toe/index.html", "text/html");
+    //   break;
+    // case "/rock-paper-scissors/index.html":
+    //   createfile("rock-paper-scissors/index.html", "text/html");
+    //   app.use(express.static("images"));
+    //   app.use("./images", express.static("images"));
+    //   break;
+    // case "/rock-paper-scissors/images/rock.png":
+    //   createfile("rock-paper-scissors/images/rock.png", "image/gif");
+    //   break;
+    // case "/rock-paper-scissors/images/paper.png":
+    //   createfile("rock-paper-scissors/images/paper.png", "image/gif");
+    //   break;
+    // case "/rock-paper-scissors/images/scissors.png":
+    //   createfile("rock-paper-scissors/images/scissors.png", "image/gif");
+    //   break;
+    // case "/images/logo.png":
+    //   createfile("images/logo.png", "image/gif");
+    //   break;
+    // case "/assets/css/images/header.svg":
+    //   createfile("assets/css/images/header.svg", "image/svg+xml");
+    //   break;
+    // case "/images/bee.png":
+    //   createfile("images/bee.png", "image/gif");
+    //   break;
+    // case "/images/rock.png":
+    //   createfile("images/rock.png", "image/gif");
+    //   break;
+    // case "/images/snake.png":
+    //   createfile("images/snake.png", "image/gif");
+    //   break;
+    // case "/images/tic.png":
+    //   createfile("images/tic.png", "image/gif");
+    //   break;
+    // case "/api":
+    //   res.writeHead(200, { "Content-Type": "application/json" });
 
-      if ("student" in params) {
-        let adjArray = [
-          "colorful",
-          "happy",
-          "enthusiastic",
-          "agreeable",
-          "funky",
-        ];
-        let verbArray = ["dancing", "walking", "working", "coding", "playing"];
-        if (adjArray.includes(params["student"])) {
-          let num = Math.floor(Math.random() * adjArray.length);
-          const objToJson = {
-            name: adjArray[num],
-            status: verbArray[num],
-            currentOccupation: "Leon",
-          };
-          res.end(JSON.stringify(objToJson));
-        } else if (params["student"] == "game") {
-          const objToJson = {
-            name: "tic-tac-toe",
-            status: "/tic-tac-toe/index.html",
-            currentOccupation: "play both",
-          };
-          res.end(JSON.stringify(objToJson));
-        } else if (
-          params["student"] == "rock" ||
-          params["student"] == "paper" ||
-          params["student"] == "scissors"
-        ) {
-          const objToJson = {
-            name: "rock-paper-scissors",
-            status: "/rock-paper-scissors/index.html",
-            currentOccupation: "play both",
-          };
-          res.end(JSON.stringify(objToJson));
-        } else {
-          const objToJson = {
-            name: "unknown",
-            status: "unknown",
-            currentOccupation: "unknown",
-          };
-          res.end(JSON.stringify(objToJson));
-        }
+    //   if ("student" in params) {
+    //     let adjArray = [
+    //       "colorful",
+    //       "happy",
+    //       "enthusiastic",
+    //       "agreeable",
+    //       "funky",
+    //     ];
+    //     let verbArray = ["dancing", "walking", "working", "coding", "playing"];
+    //     if (adjArray.includes(params["student"])) {
+    //       let num = Math.floor(Math.random() * adjArray.length);
+    //       const objToJson = {
+    //         name: adjArray[num],
+    //         status: verbArray[num],
+    //         currentOccupation: "Leon",
+    //       };
+    //       res.end(JSON.stringify(objToJson));
+    //     } else if (params["student"] == "game") {
+    //       const objToJson = {
+    //         name: "tic-tac-toe",
+    //         status: "/tic-tac-toe/index.html",
+    //         currentOccupation: "play both",
+    //       };
+    //       res.end(JSON.stringify(objToJson));
+    //     } else if (
+    //       params["student"] == "rock" ||
+    //       params["student"] == "paper" ||
+    //       params["student"] == "scissors"
+    //     ) {
+    //       const objToJson = {
+    //         name: "rock-paper-scissors",
+    //         status: "/rock-paper-scissors/index.html",
+    //         currentOccupation: "play both",
+    //       };
+    //       res.end(JSON.stringify(objToJson));
+    //     } else {
+    //       const objToJson = {
+    //         name: "unknown",
+    //         status: "unknown",
+    //         currentOccupation: "unknown",
+    //       };
+    //       res.end(JSON.stringify(objToJson));
+    //     }
+    //   }
+
+    //   break;
+    // case "/css/style.css":
+    //   fs.readFile("css/style.css", (err, data) => {
+    //     res.write(data);
+    //     res.end();
+    //   });
+    //   break;
+    // case "/assets/css/main.css":
+    //   fs.readFile("assets/css/main.css", (err, data) => {
+    //     res.write(data);
+    //     res.end();
+    //    });
+    //   break;
+    // case "/tic-tac-toe/style.css":
+    //   fs.readFile("tic-tac-toe/style.css", (err, data) => {
+    //     res.write(data);
+    //     res.end();
+    //   });
+    //   break;
+    // case "/rock-paper-scissors/style.css":
+    //   fs.readFile("rock-paper-scissors/style.css", (err, data) => {
+    //     res.write(data);
+    //     res.end();
+    //   });
+    //   break;
+    // case "/js/main.js":
+    //   createfile("js/main.js", "text/javascript");
+    //   break;
+    // case "/assets/js/main.js":
+    //   createfile("assets/js/main.js", "text/javascript");
+    //   break;
+    // case "/assets/js/breakpoints.min.js":
+    //   createfile("assets/js/breakpoints.min.js", "text/javascript");
+    //   break;
+    // case "/assets/js/browser.min.js":
+    //   createfile("assets/js/browser.min.js", "text/javascript");
+    //   break;
+    // case "/assets/js/jquery.dropotron.min.js":
+    //   createfile("assets/js/jquery.dropotron.min.js", "text/javascript");
+    //   break;
+    // case "/assets/js/jquery.min.js":
+    //   createfile("assets/js/jquery.min.js", "text/javascript");
+    //   break;
+    // case "/assets/js/util.js":
+    //   createfile("assets/js/util.js", "text/javascript");
+    //   break;
+    // case "/tic-tac-toe/app.js":
+    //   createfile("tic-tac-toe/app.js", "text/javascript");
+    //   break;
+    // case "/rock-paper-scissors/app.js":
+    //   createfile("rock-paper-scissors/app.js", "text/javascript");
+    //   break;
+   // default:
+      // figlet("404!!", function (err, data) {
+      //   if (err) {
+      //     console.log("Something went wrong...");
+      //     console.dir(err);
+      //     return;
+      //   }
+      //   res.write(data);
+      //   res.end();
+      // });
+      default:
+        if (page.endsWith('/')) {
+          route(res, "/index.html", "text/html");
+      } else if (page.endsWith('.html')) {
+          route(res, page, "text/html");
+      } else if (page.endsWith('.js')) {
+          route(res, page, "text/javascript");
+      } else if (page.endsWith('.css')) {
+          route(res, page, "text/css");
+      } else if (page.endsWith('.png')) {
+          route(res, page, "image/png");
+      } else if (page.endsWith('.jpg')) {
+          route(res, page, "image/jpeg");
+      } else if (page.endsWith('.gif')) {
+          route(res, page, "image/gif");
+      } else {
+        figlet("404!!", function (err, data) {
+          if (err) {
+            console.log("Something went wrong...");
+            console.dir(err);
+            return;
+          }
+          res.write(data);
+          res.end();
+        });
       }
-
-      break;
-    case "/css/style.css":
-      fs.readFile("css/style.css", (err, data) => {
+    }
+  });
+  
+  function route(res, page, contentType) {
+    let destination = page.replace('/', ""); // Remove first slash
+    fs.readFile(destination, function (err, data) {
+        res.writeHead(200, { "Content-Type": contentType });
         res.write(data);
         res.end();
-      });
-      break;
-    case "/assets/css/main.css":
-      fs.readFile("assets/css/main.css", (err, data) => {
-        res.write(data);
-        res.end();
-       });
-      break;
-    case "/tic-tac-toe/style.css":
-      fs.readFile("tic-tac-toe/style.css", (err, data) => {
-        res.write(data);
-        res.end();
-      });
-      break;
-    case "/rock-paper-scissors/style.css":
-      fs.readFile("rock-paper-scissors/style.css", (err, data) => {
-        res.write(data);
-        res.end();
-      });
-      break;
-    case "/js/main.js":
-      createfile("js/main.js", "text/javascript");
-      break;
-    case "/assets/js/main.js":
-      createfile("assets/js/main.js", "text/javascript");
-      break;
-    case "/assets/js/breakpoints.min.js":
-      createfile("assets/js/breakpoints.min.js", "text/javascript");
-      break;
-    case "/assets/js/browser.min.js":
-      createfile("assets/js/browser.min.js", "text/javascript");
-      break;
-    case "/assets/js/jquery.dropotron.min.js":
-      createfile("assets/js/jquery.dropotron.min.js", "text/javascript");
-      break;
-    case "/assets/js/jquery.min.js":
-      createfile("assets/js/jquery.min.js", "text/javascript");
-      break;
-    case "/assets/js/util.js":
-      createfile("assets/js/util.js", "text/javascript");
-      break;
-    case "/tic-tac-toe/app.js":
-      createfile("tic-tac-toe/app.js", "text/javascript");
-      break;
-    case "/rock-paper-scissors/app.js":
-      createfile("rock-paper-scissors/app.js", "text/javascript");
-      break;
-    default:
-      figlet("404!!", function (err, data) {
-        if (err) {
-          console.log("Something went wrong...");
-          console.dir(err);
-          return;
-        }
-        res.write(data);
-        res.end();
-      });
+    })
   }
-});
+//  }
+//});
 
 // port is dynamic based on env
 server.listen(port);
